baebbdb771eb1e0f60819b6c8a908da83e9b15d8
Frank Lai <frank810618@gmail.com>
2020-08-17 11:57:53 +0800

    splitting the vendors from main bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 var webpack = require('webpack');
 var path = require('path');
 
+const VENDOR_LIBS = [
+  'faker', 'lodash', 'redux', 'react-redux',
+  'react-dom', 'react-input-range', 'redux-form',
+  'redux-thunk', 'react-router', 'react'
+];
+
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js' // [name] will be replaced by entry's key
   },
   module: {
     rules: [
@@ -15,8 +24,8 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-          use: ['style-loader', 'css-loader'],  // the order is important, dealed from right to left
-          test: /\.css$/
+        use: ['style-loader', 'css-loader'],  // the order is important, dealed from right to left
+        test: /\.css$/
       }
     ]
   }
