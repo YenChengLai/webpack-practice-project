5cc203d29e5acafaf2cd3fe57d654fe492c5f8fc
Frank Lai <frank810618@gmail.com>
2020-08-19 22:27:49 +0800

    webpack middleware in production

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const webpackMiddleWare = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config');
+const path = require('path');
 
 const app = express();
 
-app.use(webpackMiddleWare(webpack(webpackConfig))); // tell express to use the webpack middleware
+if (process.env.NODE_ENV !== 'production') { // not in production => use webpack
+    const webpackMiddleWare = require('webpack-dev-middleware');
+    const webpack = require('webpack');
+    const webpackConfig = require('./webpack.config');
+    app.use(webpackMiddleWare(webpack(webpackConfig))); // tell express to use the webpack middleware
+} else {
+    app.use(express.static('dist')); // open up dist dir with express
+    app.get('*', (req, res) => { // react router
+        res.sendFile(path.join(__dirname, 'dist/index.html'));
+    });
+}
 
-app.listen(3050, () => console.log('Listening'));
+app.listen(process.env.PORT || 3050, () => console.log('Listening')); // use provider port or 3050 port
